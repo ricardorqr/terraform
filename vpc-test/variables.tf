1aee4bd698fbc3ed14804c3ca68db495bd1f9f32
Ricardo Ribeiro <ricarrib@amdocs.com>
2023-03-12 19:09:06 -0400

    Update main script to use 1 public subnet and 1 private subnet

--- vpc-test/variables.tf
+++ vpc-test/variables.tf
@@ -3,4 +3,6 @@ variable profile {}
 variable project-name {}
 variable vpc-cidr {}
 variable public-subnet-a-cidr {}
+variable public-subnet-b-cidr {}
 variable private-subnet-a-cidr {}
+variable private-subnet-b-cidr {}
