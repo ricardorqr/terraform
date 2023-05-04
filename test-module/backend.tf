d939a5c3df34f4ec3767564723b8f7f717d6e8f8
Ricardo Ribeiro <ricardo.ribeiro@veeva.com>
2023-05-04 12:13:32 -0400

    Delete test

--- test-module/backend.tf
+++
@@ -1,9 +0,0 @@
-# store the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket  = "terraform-remote-state-rico"
-    key     = "test-module.tfstate"
-    region  = "us-east-1"
-    profile = "stg-ext"
-  }
-}
