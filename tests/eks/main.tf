6d93909446dea17972e0522cb86e944bb2f4a404
Ricardo Ribeiro <ricardo.ribeiro@veeva.com>
2023-05-08 14:22:24 -0400

    Add EKS module test

---
+++ tests/eks/main.tf
@@ -0,0 +1,37 @@
+module "vpc" {
+  source = "../../modules/vpc/v1"
+
+  profile               = var.profile
+  project-name          = var.project-name
+  env                   = var.env
+  vpc-cidr              = var.vpc-cidr
+  region                = var.region
+  azs                   = var.azs
+  public-subnet-cidrs   = var.public-subnet-cidrs
+  private-subnet-cidrs  = var.private-subnet-cidrs
+  database-subnet-cidrs = var.database-subnet-cidrs
+}
+
+module "vpc-sg" {
+  source = "../../modules/sg/vpc/v1"
+
+  profile      = var.profile
+  project-name = var.project-name
+  env          = var.env
+  vpc-id       = module.vpc.vpc-id
+  region       = var.region
+}
+
+module "eks" {
+  source = "../../modules/eks/v1"
+
+  profile            = var.profile
+  project-name       = var.project-name
+  env                = var.env
+  region             = var.region
+  azs                = var.azs
+  vpc-id             = module.vpc.vpc-id
+  private-subnet-ids = module.vpc.private-subnets
+  node-type          = var.node-type
+  kubernetes-version = var.kubernetes-version
+}
---
+++ tests/eks/variables.tf
@@ -0,0 +1,16 @@
+# AWS
+variable profile {}
+variable project-name {}
+variable env {}
+
+# VPC
+variable region {}
+variable azs {}
+variable vpc-cidr {}
+variable public-subnet-cidrs {}
+variable private-subnet-cidrs {}
+variable database-subnet-cidrs {}
+
+# EKS
+variable node-type {}
+variable kubernetes-version {}
