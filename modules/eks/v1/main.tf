8b035daf99d66b8d661c605bcd889ac221358fe3
Ricardo Ribeiro <ricardo.ribeiro@veeva.com>
2023-05-08 20:55:18 -0400

    Update EKS module

--- modules/eks/v1/main.tf
+++ modules/eks/v1/main.tf
@@ -1,3 +1,29 @@
+############
+# Providers
+############
+
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.66.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "= 1.14.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "= 2.10"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "= 2.7"
+    }
+  }
+}
+
 ######
 # EKS
 ######
@@ -27,18 +53,19 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.13.1"
 
-  cluster_name                    = var.project-name
-  cluster_version                 = var.kubernetes-version
-  vpc_id                          = var.vpc-id
-  subnet_ids                      = var.private-subnet-ids
-  enable_irsa                     = true
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
-   iam_role_name                   = "${var.project-name}-eks-role"
-
+  cluster_name                        = var.project-name
+  cluster_version                     = var.kubernetes-version
+  vpc_id                              = var.vpc-id
+  subnet_ids                          = var.private-subnet-ids
+  enable_irsa                         = true
+  cluster_endpoint_private_access     = true
+  cluster_endpoint_public_access      = true
+  manage_aws_auth_configmap           = true
   node_security_group_use_name_prefix = false
-  node_security_group_tags            = {
-    "karpenter.sh/discovery/${var.project-name}" = var.project-name
+  iam_role_name                       = "${var.project-name}-eks-role"
+
+  node_security_group_tags = {
+    "karpenter.sh/discovery" = var.project-name
   }
 
   cluster_addons = {
@@ -56,12 +83,23 @@ module "eks" {
     }
   }
 
+  aws_auth_roles = [
+    {
+      rolearn  = module.karpenter.role_arn
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups   = [
+        "system:bootstrappers",
+        "system:nodes",
+      ]
+    },
+  ]
+
   eks_managed_node_groups = {
     "worker-node" = {
       name                 = "${var.project-name}-ng"
       min_size             = 2
       desired_size         = 2
-      max_size             = 50
+      max_size             = 100
       instance_types       = [var.node-type]
       subnet_ids           = var.private-subnet-ids
       tags                 = local.tags
@@ -108,6 +146,193 @@ module "eks" {
   }
 
   tags = {
-    "karpenter.sh/discovery/${var.project-name}" = var.project-name
+    "karpenter.sh/discovery" = var.project-name
   }
+}
+
+############
+# Karpenter
+############
+
+# https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest/submodules/karpenter
+module "karpenter" {
+  source  = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "19.13.1"
+
+  cluster_name                    = module.eks.cluster_name
+  irsa_oidc_provider_arn          = module.eks.oidc_provider_arn
+  irsa_namespace_service_accounts = ["karpenter:karpenter"]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.project-name, "--region", var.region]
+    }
+  }
+}
+
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+  name             = "karpenter"
+  repository       = "oci://public.ecr.aws/karpenter"
+  chart            = "karpenter"
+  version          = "v0.27.3"
+
+  set {
+    name  = "settings.aws.clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "settings.aws.clusterEndpoint"
+    value = module.eks.cluster_endpoint
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter.irsa_arn
+  }
+
+  set {
+    name  = "settings.aws.defaultInstanceProfile"
+    value = module.karpenter.instance_profile_name
+  }
+
+  set {
+    name  = "settings.aws.interruptionQueueName"
+    value = module.karpenter.queue_name
+  }
+}
+
+#############
+# YAML Files
+#############
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.project-name, "--region", var.region]
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.project-name, "--region", var.region]
+    command     = "aws"
+  }
+}
+
+resource "kubectl_manifest" "karpenter_provisioner_template" {
+  yaml_body  = <<-YAML
+  apiVersion: karpenter.k8s.aws/v1alpha1
+  kind: AWSNodeTemplate
+  metadata:
+    name: default
+  spec:
+    subnetSelector:
+      karpenter.sh/discovery: ${var.project-name}
+    securityGroupSelector:
+      karpenter.sh/discovery: ${var.project-name}
+    tags:
+      karpenter.sh/discovery: ${var.project-name}
+    blockDeviceMappings:
+      - deviceName: /dev/xvda
+        ebs:
+          volumeSize: 150Gi
+          volumeType: gp3
+          encrypted: true
+  YAML
+  depends_on = [helm_release.karpenter]
+}
+
+resource "kubectl_manifest" "karpenter_provisioner" {
+  yaml_body  = <<-YAML
+  apiVersion: karpenter.sh/v1alpha5
+  kind: Provisioner
+  metadata:
+    name: default
+  spec:
+    consolidation:
+      enabled: true
+    providerRef:
+      name: default
+    requirements:
+      - key: karpenter.sh/capacity-type
+        operator: In
+        values: ["on-demand"]
+    limits:
+      resources:
+        cpu: 1000
+#    ttlSecondsAfterEmpty: 30
+    ttlSecondsUntilExpired: 2592000
+  YAML
+  depends_on = [helm_release.karpenter]
+}
+
+################################
+# Karpenter Node not ready role
+################################
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_policy" "node-not-ready-policy" {
+  name        = "${var.project-name}-Karpenter-Node-NotReady-policy"
+  description = "Used in Karpenter"
+  path        = "/"
+  policy      = <<EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "EBS1",
+            "Effect": "Allow",
+            "Action": [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:ReEncrypt*",
+                "kms:GenerateDataKey*",
+                "kms:CreateGrant",
+                "kms:DescribeKey"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "kms:ViaService": "ec2.${var.region}.amazonaws.com",
+                    "kms:CallerAccount": "${data.aws_caller_identity.current.account_id}"
+                }
+            }
+        },
+        {
+            "Sid": "EBS2",
+            "Effect": "Allow",
+            "Action": [
+                "kms:DescribeCustomKeyStores",
+                "kms:DescribeKey",
+                "kms:Get*",
+                "kms:List*",
+                "kms:RevokeGrant"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOT
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter-attach-policy" {
+  role       = module.karpenter.irsa_name
+  policy_arn = aws_iam_policy.node-not-ready-policy.arn
 }
