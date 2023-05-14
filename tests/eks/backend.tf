f90cc5c6040f025d1b4cb7239ed8d0fc08b742f7
Ricardo Ribeiro <ricardorqr@gmail.com>
2023-05-13 20:50:55 -0400

    Update backend.tf

--- tests/eks/backend.tf
+++ tests/eks/backend.tf
@@ -1,9 +1,9 @@
-## store the terraform state file in s3
-#terraform {
-#  backend "s3" {
-#    bucket  = "devwithrico-terraform-remote-state"
-#    key     = "modules/vpc/2tiers/v1/test-vpc.tfstate"
-#    region  = "us-east-1"
-#    profile = "devwithrico"
-#  }
-#}
+# store the terraform state file in s3
+terraform {
+  backend "s3" {
+    bucket  = "devwithrico-terraform-remote-state"
+    key     = "terraform/tests/eks/eks-karpenter-vpc.tfstate"
+    region  = "us-east-1"
+    profile = "devwithrico"
+  }
+}
