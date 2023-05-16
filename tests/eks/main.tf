c2ba90299661f0d8db5cd97851c93690c7bb520f
Ricardo Ribeiro <ricardorqr@gmail.com>
2023-05-16 18:37:09 -0400

    Removed  SG and add outputs

--- tests/eks/main.tf
+++ tests/eks/main.tf
@@ -12,30 +12,27 @@ module "vpc" {
   database-subnet-cidrs = var.database-subnet-cidrs
 }
 
-module "vpc-sg" {
-  source = "../../modules/sg/vpc/v1"
-
-  profile      = var.profile
-  project-name = var.project-name
-  env          = var.env
-  vpc-id       = module.vpc.vpc-id
-  region       = var.region
-}
-
 module "eks" {
   source = "../../modules/eks/v1"
 
-  profile            = var.profile
-  project-name       = var.project-name
-  env                = var.env
-  region             = var.region
-  azs                = var.azs
-  vpc-id             = module.vpc.vpc-id
-  private-subnet-ids = module.vpc.private-subnets
-  node-type          = var.node-type
-  kubernetes-version = var.kubernetes-version
+  profile              = var.profile
+  project-name         = var.project-name
+  env                  = var.env
+  region               = var.region
+  azs                  = var.azs
+  vpc-id               = module.vpc.vpc-id
+  private-subnet-ids   = module.vpc.private-subnets
+  node-type            = var.node-type
+  kubernetes-version   = var.kubernetes-version
+  domain-application   = var.domain-application
+  veeva-hosted-zone-id = var.veeva-hosted-zone-id
 }
 
 output "eks-oidc-arn" {
   value = module.eks.oidc_provider_arn
 }
+
+output "hosted-zone" {
+  value = module.eks.hosted-zone
+}
+
