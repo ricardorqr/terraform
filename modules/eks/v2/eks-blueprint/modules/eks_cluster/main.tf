4fce21e70100073321f0bc5a812159528c3ed1d1
Ricardo Ribeiro <ricardorqr@gmail.com>
2024-02-19 16:44:48 -0500

    Add dev team

--- modules/eks/v2/eks-blueprint/modules/eks_cluster/main.tf
+++ modules/eks/v2/eks-blueprint/modules/eks_cluster/main.tf
@@ -190,99 +190,99 @@ module "eks_blueprints_platform_teams" {
   tags = local.tags
 }
 
-#module "eks_blueprints_dev_teams" {
-#  source  = "aws-ia/eks-blueprints-teams/aws"
-#  version = "~> 0.2"
-#
-#  for_each = {
-#    burnham = {
-#      labels = {
-#        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
-#        "appName"                                 = "burnham-team-app",
-#        "projectName"                             = "project-burnham",
-#      }
-#    }
-#    riker = {
-#      labels = {
-#        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
-#        "appName"                                 = "riker-team-app",
-#        "projectName"                             = "project-riker",
-#      }
-#    }
-#  }
-#  name = "team-${each.key}"
-#
-#  users             = [data.aws_caller_identity.current.arn]
-#  cluster_arn       = module.eks.cluster_arn
-#  oidc_provider_arn = module.eks.oidc_provider_arn
-#
-#  labels = merge(
-#    {
-#      team = each.key
-#    },
-#    try(each.value.labels, {})
-#  )
-#
-#  annotations = {
-#    team = each.key
-#  }
-#
-#  namespaces = {
-#    "team-${each.key}" = {
-#      labels = merge(
-#        {
-#          team = each.key
-#        },
-#        try(each.value.labels, {})
-#      )
-#
-#      resource_quota = {
-#        hard = {
-#          "requests.cpu"    = "100",
-#          "requests.memory" = "20Gi",
-#          "limits.cpu"      = "200",
-#          "limits.memory"   = "50Gi",
-#          "pods"            = "30",
-#          "secrets"         = "10",
-#          "services"        = "20"
-#        }
-#      }
-#
-#      limit_range = {
-#        limit = [
-#          {
-#            type = "Pod"
-#            max  = {
-#              cpu    = "2"
-#              memory = "1Gi"
-#            }
-#            min = {
-#              cpu    = "10m"
-#              memory = "4Mi"
-#            }
-#          },
-#          {
-#            type = "PersistentVolumeClaim"
-#            min  = {
-#              storage = "24M"
-#            }
-#          },
-#          {
-#            type    = "Container"
-#            default = {
-#              cpu    = "50m"
-#              memory = "24Mi"
-#            }
-#          }
-#        ]
-#      }
-#    }
-#  }
-#
-#  tags = local.tags
-#
-#}
-#
+module "eks_blueprints_dev_teams" {
+  source  = "aws-ia/eks-blueprints-teams/aws"
+  version = "~> 0.2"
+
+  for_each = {
+    burnham = {
+      labels = {
+        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+        "appName"                                 = "burnham-team-app",
+        "projectName"                             = "project-burnham",
+      }
+    }
+    riker = {
+      labels = {
+        "elbv2.k8s.aws/pod-readiness-gate-inject" = "enabled",
+        "appName"                                 = "riker-team-app",
+        "projectName"                             = "project-riker",
+      }
+    }
+  }
+  name = "team-${each.key}"
+
+  users             = [data.aws_caller_identity.current.arn]
+  cluster_arn       = module.eks.cluster_arn
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  labels = merge(
+    {
+      team = each.key
+    },
+    try(each.value.labels, {})
+  )
+
+  annotations = {
+    team = each.key
+  }
+
+  namespaces = {
+    "team-${each.key}" = {
+      labels = merge(
+        {
+          team = each.key
+        },
+        try(each.value.labels, {})
+      )
+
+      resource_quota = {
+        hard = {
+          "requests.cpu"    = "100",
+          "requests.memory" = "20Gi",
+          "limits.cpu"      = "200",
+          "limits.memory"   = "50Gi",
+          "pods"            = "30",
+          "secrets"         = "10",
+          "services"        = "20"
+        }
+      }
+
+      limit_range = {
+        limit = [
+          {
+            type = "Pod"
+            max  = {
+              cpu    = "2"
+              memory = "1Gi"
+            }
+            min = {
+              cpu    = "10m"
+              memory = "4Mi"
+            }
+          },
+          {
+            type = "PersistentVolumeClaim"
+            min  = {
+              storage = "24M"
+            }
+          },
+          {
+            type    = "Container"
+            default = {
+              cpu    = "50m"
+              memory = "24Mi"
+            }
+          }
+        ]
+      }
+    }
+  }
+
+  tags = local.tags
+
+}
+
 #module "kubernetes_addons" {
 #  source = "github.com/aws-ia/terraform-aws-eks-blueprints?ref=v4.32.1/modules/kubernetes-addons"
 #
